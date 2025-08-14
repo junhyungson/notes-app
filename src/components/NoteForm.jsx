00695258748f153331dcf433ddcb8b608491f950
junhyungson <cybergeron89@gmail.com>
2025-08-14 14:34:09 -0700

    collapsible form

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -7,6 +7,7 @@ const NoteForm = ({ notes, setNotes }) => {
     priority: "Medium",
     description: "",
   });
+  const [isFormVisible, setIsFormVisible] = useState("false");
 
   const handleChange = (e) => {
     setFormData({
@@ -34,66 +35,76 @@ const NoteForm = ({ notes, setNotes }) => {
   };
 
   return (
-    <form className="mb-6" onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label htmlFor="title" className="block font-semibold">
-          Title
-        </label>
-        <input
-          name="title"
-          type="text"
-          className="w-full border rounded-lg"
-          value={formData.title}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="priority" className="block font-semibold">
-          priority
-        </label>
-        <select
-          name="priority"
-          type="text"
-          className="w-full border rounded-lg"
-          value={formData.priority}
-          onChange={handleChange}
-        >
-          <option value="High">High</option>
-          <option value="Medium">Medium</option>
-          <option value="Low">Low</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label htmlFor="category" className="block font-semibold">
-          Category
-        </label>
-        <select
-          name="category"
-          type="text"
-          className="w-full border rounded-lg"
-          value={formData.category}
-          onChange={handleChange}
-        >
-          <option value="Work">Work</option>
-          <option value="Personal">Personal</option>
-          <option value="Ideas">Ideas</option>
-        </select>
-      </div>
+    <React.Fragment>
+      <button
+        onClick={() => setIsFormVisible(!isFormVisible)}
+        className="w-full bg-gray-100 border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover: border-purple-300 transition mb-4"
+      >
+        {isFormVisible ? "Hide Form X" : "Add New Note +"}
+      </button>
+      {isFormVisible && (
+        <form className="mb-6" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-semibold">
+              Title
+            </label>
+            <input
+              name="title"
+              type="text"
+              className="w-full border rounded-lg"
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="priority" className="block font-semibold">
+              priority
+            </label>
+            <select
+              name="priority"
+              type="text"
+              className="w-full border rounded-lg"
+              value={formData.priority}
+              onChange={handleChange}
+            >
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="category" className="block font-semibold">
+              Category
+            </label>
+            <select
+              name="category"
+              type="text"
+              className="w-full border rounded-lg"
+              value={formData.category}
+              onChange={handleChange}
+            >
+              <option value="Work">Work</option>
+              <option value="Personal">Personal</option>
+              <option value="Ideas">Ideas</option>
+            </select>
+          </div>
 
-      <div className="mb-4">
-        <label htmlFor="description" className="block font-semibold">
-          Description
-        </label>
-        <textarea
-          name="description"
-          type="text"
-          className="w-full border rounded-lg"
-          value={formData.description}
-          onChange={handleChange}
-        />
-      </div>
-      <button>Add Note</button>
-    </form>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-semibold">
+              Description
+            </label>
+            <textarea
+              name="description"
+              type="text"
+              className="w-full border rounded-lg"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </div>
+          <button>Add Note</button>
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
