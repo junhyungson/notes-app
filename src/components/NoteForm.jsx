9b830fad27d792904e2d0ce9c8dedb8a8fe68417
junhyungson <cybergeron89@gmail.com>
2025-08-13 17:48:52 -0700

    initia set up form states

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+const NoteForm = () => {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("Medium");
+  const [category, setCategory] = useState("Work");
+  const [description, setDescription] = useState("");
+  return (
+    <form className="mb-6">
+      <div className="mb-4">
+        <label htmlFor="title" className="block font-semibold">
+          Title
+        </label>
+        <input
+          type="text"
+          className="w-full border rounded-lg"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="priority" className="block font-semibold">
+          priority
+        </label>
+        <select
+          type="text"
+          className="w-full border rounded-lg"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+        >
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="category" className="block font-semibold">
+          Category
+        </label>
+        <select
+          type="text"
+          className="w-full border rounded-lg"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Work">Work</option>
+          <option value="Personal">Personal</option>
+          <option value="Ideas">Ideas</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="description" className="block font-semibold">
+          Description
+        </label>
+        <textarea
+          type="text"
+          className="w-full border rounded-lg"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default NoteForm;
