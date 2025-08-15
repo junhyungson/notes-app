20d3470d725c775513b281f681183f57cff645ff
junhyungson <cybergeron89@gmail.com>
2025-08-15 13:13:21 -0700

    created note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Note = ({ note, deleteNote }) => {
+  console.log(note);
+  const getBorderColor = (priority) => {
+    const colors = {
+      High: "border-red-500",
+      Medium: "border-yellow-500",
+      Low: "border-green-500",
+    };
+    return colors[priority] || "border-gray-300";
+  };
+  const borderColor = getBorderColor(note.priority);
+  return (
+    <div
+      key={note.id}
+      className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${borderColor}`}
+    >
+      <h3 className="text-lg font-bold">{note.title}</h3>
+      <p className="text-sm text-gray-600">
+        <strong>Category:</strong> {note.category}
+      </p>
+      <p className="text-sm text-gray-600">
+        <strong>Description:</strong> {note.description}
+      </p>
+      <button
+        onClick={() => deleteNote(note.id)}
+        className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
+      >
+        🗑️Delete
+      </button>
+    </div>
+  );
+};
+
+export default Note;
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,55 +1,15 @@
 import React from "react";
+import Note from "./Note";
 
 const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className="text-center text-gray-500">No Notes Yet</p>;
   }
-
-  // const getBorderColor = (priority) => {
-  //   switch (priority) {
-  //     case "High":
-  //       return "border-red-500";
-  //     case "Medium":
-  //       return "border-yellow-500";
-  //     case "Low":
-  //       return "border-green-500";
-  //     default:
-  //       return "border-gray-300"; // fallback
-  //   }
-  // };
-  const getBorderColor = (priority) => {
-    const colors = {
-      High: "border-red-500",
-      Medium: "border-yellow-500",
-      Low: "border-green-500",
-    };
-    return colors[priority] || "border-gray-300";
-  };
   return (
     <div className="space-y-4">
-      {notes.map((note) => {
-        const borderColor = getBorderColor(note.priority);
-        return (
-          <div
-            key={note.id}
-            className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${borderColor}`}
-          >
-            <h3 className="text-lg font-bold">{note.title}</h3>
-            <p className="text-sm text-gray-600">
-              <strong>Category:</strong> {note.category}
-            </p>
-            <p className="text-sm text-gray-600">
-              <strong>Description:</strong> {note.description}
-            </p>
-            <button
-              onClick={() => deleteNote(note.id)}
-              className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
-            >
-              🗑️Delete
-            </button>
-          </div>
-        );
-      })}
+      {notes.map((note) => (
+        <Note key={note.id} note={note} deleteNote={deleteNote} />
+      ))}
     </div>
   );
 };
